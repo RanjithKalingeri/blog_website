ca40dc4233fdf8767aecf954e283585ef91a2cfe
Ranjith Kalingeri <76031174+RanjithKalingeri@users.noreply.github.com>
2021-06-30 23:27:02 +0530

    Update settings.py
    password hashers added

--- level2/ProTwo/ProTwo/settings.py
+++ level2/ProTwo/ProTwo/settings.py
@@ -98,7 +98,10 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 PASSWORD_HASHERS = [
-'django.contrib.auth.hashers.Argon2PasswordHasher'
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
 ]
 
 AUTH_PASSWORD_VALIDATORS = [
